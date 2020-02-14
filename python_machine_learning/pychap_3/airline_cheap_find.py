d38c8c42dad4affa38423adb44ecc99775a91dfe
CyborgVillager <almawijonathan@gmail.com>
2020-02-14 09:16:02 -0600

    v25 Basic Image ScreenShot
    Basic Image ScreenShot: file airline_cheap.find.py can now save the image screenshot when a user has opened
                                    the airline line of their choosing. It will then name it incrementally 0 and up.

--- python_machine_learning/pychap_3/airline_cheap_find.py
+++ python_machine_learning/pychap_3/airline_cheap_find.py
@@ -6,3 +6,32 @@ browser = webdriver.Chrome(chromedriver_path)
 sats = 'https://www.google.com/travel/explore?tfs=CBkQAxooEgoyMDIwLTAzLTAxagwIAhIIL20vMDFfZDRyDAgEEggvbS8wNTBsOBooEgoyMDIwLTAzLTA3agwIBBIIL20vMDUwbDhyDAgCEggvbS8wMV9kNEABSAFSA1VTRHAB&curr=USD&gl=us&hl=en&authuser=0&origin=https%3A%2F%2Fwww.google.com&mvb=ChIJoWKc0WWYSUARrBqEuX24WMASEgnnNUrBw8FFQBGsGoS5fXddwA'
 
 print(browser.get(sats))
+print(browser.title)
+
+# will work on incrementing file names in the near future basic test
+
+image_location = 'screenshot/'
+img_name = 'flight_image_0'
+file_format = '.png'
+'''
+img_number_counter = 0
+img_name = 'flight_image_'
+img_number_counter + 1
+img_file_show = img_name + str(img_number_counter)
+
+try:
+    browser.save_screenshot(image_location + img_file_show + '.png')
+    if img_number_counter <= 500:
+        img_number_counter + 1
+    else: print('Maxed out on picture space.')
+except:
+        print('Cant take screenshot at this time')
+
+'''
+
+try:
+    browser.save_screenshot(image_location + img_name + file_format)
+    print('Success, image is now saved @ folder screenshot. Saved as ' + img_name)
+except OSError:
+                print('Error cant save file ' + img_name + ' at this time' + 'please check the folder' + '\n' +
+                      'screenshot')
